d9e589aefcebe67b14fd5d7ad973bfc9e7c4190e
Saklain Raza <saklainraza0@gmail.com>
2025-01-22 17:54:49 +0530

    new changes2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "public", // Change the output directory to 'public'
+    outDir: "dist", // Change the output directory to 'public'
   },
 });
